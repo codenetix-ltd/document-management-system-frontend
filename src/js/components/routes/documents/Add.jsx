ddd7254683dae0a10229897826819e9ede448a54
Nurbek Abulgazin <nabulgazin@codenetix.com>
2018-05-07 19:52:26 +0600

    DOSS-57/Replace custom tabs with react-bootstrap tabs

--- src/js/components/routes/documents/Add.jsx
+++ src/js/components/routes/documents/Add.jsx
@@ -3,43 +3,27 @@ import autobind from 'autobind-decorator';
 
 import ContentHeader from 'Components/ContentHeader';
 import ContentWrapper from 'Components/ContentWrapper';
-import Tabs from 'Components/common/Tabs'; // todo: use react-bootstrap's tabs component
+import { Tabs, Tab } from 'react-bootstrap';
 
 import DocumentContent from './partials/DocumentContent';
 
 @autobind
 export default class DocumentAdd extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tabsConfig: []
-    };
-  }
-
-  componentDidMount() {
-    this.setState({
-      tabsConfig: [
-        {
-          title: 'Content',
-          active: true,
-          content: <DocumentContent />
-        }
-      ]
-    });
-  }
-
   breadcrumbs = [
     { pageName: 'Documents', pageLink: '/documents/list', iconCls: 'fa fa-copy' },
     { pageName: 'Document create', pageLink: '/documents', iconCls: 'fa fa-plus' }
   ];
 
   render() {
-    const { tabsConfig } = this.state;
     return (
       <div>
         <ContentHeader title="Document create" breadcrumbs={this.breadcrumbs} />
         <ContentWrapper noBox>
-          <Tabs list={tabsConfig} />
+          <Tabs id="tabs">
+            <Tab eventKey={1} title="Content">
+              <DocumentContent />
+            </Tab>
+          </Tabs>
         </ContentWrapper>
       </div>
     );
