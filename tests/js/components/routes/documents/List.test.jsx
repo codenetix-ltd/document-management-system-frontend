9e700246d7c41ff609213e00654ef16ef2c867e7
Nurbek Abulgazin <nabulgazin@codenetix.com>
2018-05-21 16:51:58 +0600

    DOSS-93/Move __mocks__ folder to tests directory,         move jest settings to jest.config.js,         add more tests to List.test.js

--- tests/js/components/routes/documents/List.test.jsx
+++ tests/js/components/routes/documents/List.test.jsx
@@ -5,17 +5,63 @@ import { shallowToJson } from 'enzyme-to-json';
 import { DocumentsList as List } from 'Components/routes/documents/List';
 
 describe('Documents list', () => {
-  let wrapper;
   let documents;
+  let spy;
+  let onSelectSpy;
+  let onFetchDataSpy;
+  let onArchiveSpy;
+  let onDeleteSpy;
+  let wrapper;
+  let instance;
 
   beforeEach(() => {
-    documents = {
-      list: []
+    documents = { list: [] };
+    spy = jest.fn();
+    onSelectSpy = jest.spyOn(List.prototype, 'onSelect');
+    onFetchDataSpy = jest.spyOn(List.prototype, 'onFetchData');
+    onArchiveSpy = jest.spyOn(List.prototype, 'onArchive');
+    onDeleteSpy = jest.spyOn(List.prototype, 'onDelete');
+    wrapper = shallow(<List documents={documents} dispatch={spy} loading={false} />);
+    instance = wrapper.instance();
+    instance.prompt = {
+      show: () => {}
     };
-    wrapper = shallow(<List documents={documents} dispatch={() => {}} loading={false} />);
   });
 
   it('should render correctly', () => {
     expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('should call onFetchData', () => {
+    instance.onFetchData({
+      page: 1,
+      sorted: [{ id: 1, desc: true }]
+    });
+    expect(onFetchDataSpy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should call onArchive', () => {
+    instance.onArchive({
+      value: {
+        actualVersion: {}
+      }
+    });
+    expect(onArchiveSpy).toHaveBeenCalled();
+  });
+
+  it('should call onArchive', () => {
+    instance.onDelete({
+      value: {
+        actualVersion: {}
+      }
+    });
+    expect(onDeleteSpy).toHaveBeenCalled();
+  });
+
+  it('should call onSelect', () => {
+    instance.onSelect();
+    expect(onSelectSpy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalled();
   });
 });
