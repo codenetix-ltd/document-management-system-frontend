a24128776cf67cc83c6b515ae901d90f4e1ba837
Nurbek Abulgazin <nabulgazin@codenetix.com>
2018-05-23 20:03:11 +0600

    DOSS-93/Add more tests to Documents AttributesForm suite

--- tests/js/components/routes/documents/partials/AttributesForm.test.jsx
+++ tests/js/components/routes/documents/partials/AttributesForm.test.jsx
@@ -4,22 +4,60 @@ import { shallowToJson } from 'enzyme-to-json';
 
 import { AttributesForm } from 'Routes/documents/partials/AttributesForm';
 
+import { $updateAttributeValues } from 'Store/actions';
+
+jest.mock('Store/actions', () => ({
+  $updateAttributeValues: jest.fn()
+}));
+
 describe('Documents AttributesForm', () => {
   let wrapper;
-  let document;
+  let props;
+  let instance;
 
   beforeEach(() => {
-    document = {
-      actualVersion: {
-        template: {
-          attributes: []
+    props = {
+      dispatch: () => {},
+      document: {
+        actualVersion: {
+          attributeValues: [],
+          template: {
+            attributes: []
+          }
         }
       }
     };
-    wrapper = shallow(<AttributesForm dispatch={() => {}} document={document} />);
+    wrapper = shallow(<AttributesForm {...props} />);
+    instance = wrapper.instance();
   });
 
   it('should render correctly', () => {
     expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('should call $updateAttributeValues upon calling onAttrChange', () => {
+    instance.onAttrChange(
+      {
+        target: {
+          value: ''
+        }
+      }, {
+        id: 1
+      }
+    );
+    expect($updateAttributeValues.mock.calls.length).toBe(1);
+  });
+
+  it('should call $updateAttributeValues upon calling onCheckboxToggle', () => {
+    instance.onCheckboxToggle(
+      {
+        target: {
+          value: ''
+        }
+      }, {
+        id: 1
+      }
+    );
+    expect($updateAttributeValues.mock.calls.length).toBe(1);
   });
 });
