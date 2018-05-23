55a30c27ef0b2bdc252fedb8cf80bc57abe7812b
Nurbek Abulgazin <nabulgazin@codenetix.com>
2018-05-23 19:44:32 +0600

    DOSS-93/Add more tests to Documents ArchiveModalContent suite

--- tests/js/components/routes/documents/partials/ArchiveModalContent.test.jsx
+++ tests/js/components/routes/documents/partials/ArchiveModalContent.test.jsx
@@ -4,25 +4,38 @@ import { shallowToJson } from 'enzyme-to-json';
 
 import { ArchiveModalContent } from 'Routes/documents/partials/ArchiveModalContent';
 
+import { $substituteDocument } from 'Store/actions';
+
+jest.mock('Store/actions', () => ({
+  $substituteDocument: jest.fn()
+}));
+
 describe('Documents ArchiveModalContent', () => {
   let wrapper;
-  let documents;
-  let subsDoc;
+  let props;
+  let instance;
 
   beforeEach(() => {
-    subsDoc = {};
-    documents = {
-      list: []
+    props = {
+      dispatch: () => {},
+      documents: {
+        list: []
+      },
+      selectedDocuments: [],
+      substituteDocument: {}
     };
-    wrapper = shallow(<ArchiveModalContent
-      dispatch={() => {}}
-      documents={documents}
-      selectedDocuments={[]}
-      substituteDocument={subsDoc}
-    />);
+    wrapper = shallow(<ArchiveModalContent {...props} />);
+    instance = wrapper.instance();
   });
 
   it('should render correctly', () => {
     expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('should call $substituteDocument upon calling onChange', () => {
+    instance.onChange('test');
+    expect($substituteDocument).toBeCalled();
+    expect($substituteDocument).toBeCalledWith('test');
+    expect($substituteDocument.mock.calls.length).toBe(1);
   });
 });
