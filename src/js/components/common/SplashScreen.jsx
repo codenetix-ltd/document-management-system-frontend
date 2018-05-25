f81d59b606b9a44682c4210903c9c8b50974da06
Nurbek Abulgazin <nabulgazin@codenetix.com>
2018-05-25 14:47:49 +0600

    DOSS-93/Add SplashScreen for loadable components, remove Tabs component

---
+++ src/js/components/common/SplashScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SplashScreen() {
+  return (
+    <div className="splash-screen text-center">
+      Loading...
+    </div>
+  );
+}
--- src/js/components/routes/index.jsx
+++ src/js/components/routes/index.jsx
@@ -1,157 +1,159 @@
 import Loadable from 'react-loadable';
 import React from 'react';
 
+import SplashScreen from 'Components/common/SplashScreen';
+
 export const Home = Loadable({
   loader: () => import('Routes/home'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const UserAdd = Loadable({
   loader: () => import('Routes/users/Add'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const UserEdit = Loadable({
   loader: () => import('Routes/users/Edit'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const UsersList = Loadable({
   loader: () => import('Routes/users/List'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const RoleAdd = Loadable({
   loader: () => import('Routes/roles/Add'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const RoleEdit = Loadable({
   loader: () => import('Routes/roles/Edit'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const RolesList = Loadable({
   loader: () => import('Routes/roles/List'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const LabelAdd = Loadable({
   loader: () => import('Routes/labels/Add'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const LabelEdit = Loadable({
   loader: () => import('Routes/labels/Edit'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const LabelsList = Loadable({
   loader: () => import('Routes/labels/List'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const TemplateAdd = Loadable({
   loader: () => import('Routes/templates/Add'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const TemplateEdit = Loadable({
   loader: () => import('Routes/templates/Edit'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const TemplatesList = Loadable({
   loader: () => import('Routes/templates/List'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const AttributesAdd = Loadable({
   loader: () => import('Routes/templates/attributes/Add'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const AttributesEdit = Loadable({
   loader: () => import('Routes/templates/attributes/Edit'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const DocumentAdd = Loadable({
   loader: () => import('Routes/documents/Add'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const DocumentEdit = Loadable({
   loader: () => import('Routes/documents/Edit'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const DocumentsList = Loadable({
   loader: () => import('Routes/documents/List'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const DocumentView = Loadable({
   loader: () => import('Routes/documents/View'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const DocumentsCompare = Loadable({
   loader: () => import('Routes/documents/Compare'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const LogsList = Loadable({
   loader: () => import('Routes/logs/List'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
 export const ProfileEdit = Loadable({
   loader: () => import('Routes/profile/Edit'),
   loading() {
-    return <div>Loading...</div>;
+    return <SplashScreen />;
   }
 });
 
