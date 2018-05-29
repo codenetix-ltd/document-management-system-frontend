64ee8cc2e8391b34d6b59fd62acb42376f5c3755
Nurbek <nurbek.ab@gmail.com>
2018-05-30 00:43:05 +0600

    DOSS-93/Add more tests to FiltersWrapper suite

--- tests/js/components/routes/documents/partials/FiltersWrapper.test.jsx
+++ tests/js/components/routes/documents/partials/FiltersWrapper.test.jsx
@@ -4,31 +4,94 @@ import { shallowToJson } from 'enzyme-to-json';
 
 import { FiltersWrapper } from 'Routes/documents/partials/FiltersWrapper';
 
+import { $$templatesFetch } from 'Store/thunks/templates';
+import { $$documentsFetch } from 'Store/thunks/documents';
+
+jest.mock('Store/thunks/templates', () => ({
+  $$templatesFetch: jest.fn()
+}));
+
+jest.mock('Store/thunks/documents', () => ({
+  $$documentsFetch: jest.fn()
+}));
+
 describe('Documents FiltersWrapper', () => {
   let wrapper;
-  let documents;
-  let templates;
-  let labels;
+  let props;
+  let instance;
+  let dispatch;
 
   beforeEach(() => {
-    documents = {
-      list: []
+    dispatch = jest.fn();
+    props = {
+      dispatch,
+      documents: {
+        list: []
+      },
+      templates: {
+        list: []
+      },
+      labels: {
+        list: []
+      }
     };
-    templates = {
-      list: []
-    };
-    labels = {
-      list: []
-    };
-    wrapper = shallow(<FiltersWrapper
-      dispatch={() => {}}
-      documents={documents}
-      templates={templates}
-      labels={labels}
-    />);
+    wrapper = shallow(<FiltersWrapper {...props} />);
+    instance = wrapper.instance();
   });
 
   it('should render correctly', () => {
     expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('should call $$templatesFetch on componentDidMount', () => {
+    expect($$templatesFetch.mock.calls.length).toBe(1);
+  });
+
+  it('should change templateOptions state on componentWillReceiveProps', () => {
+    const nextProps = {
+      templates: {
+        list: [
+          { id: 1, name: 'Lorem' },
+          { id: 2, name: 'Ipsum' }
+        ]
+      }
+    };
+    expect(instance.state).toHaveProperty('templateOptions');
+    expect(instance.state.templateOptions).toBeInstanceOf(Array);
+    expect(instance.state.templateOptions.length).toBe(0);
+    wrapper.setProps(nextProps, () => {
+      expect(instance.state.templateOptions.length).toBe(2);
+    });
+  });
+
+  it('should change labelOptions state on componentWillReceiveProps', () => {
+    const nextProps = {
+      labels: {
+        list: [
+          { id: 1, name: 'Lorem' },
+          { id: 2, name: 'Ipsum' }
+        ]
+      }
+    };
+    expect(instance.state).toHaveProperty('labelOptions');
+    expect(instance.state.labelOptions).toBeInstanceOf(Array);
+    expect(instance.state.labelOptions.length).toBe(0);
+    wrapper.setProps(nextProps, () => {
+      expect(instance.state.labelOptions.length).toBe(2);
+    });
+  });
+
+  it('should call dispatch when calling onFilterChange', () => {
+    expect(dispatch).not.toBeCalled();
+    instance.onFilterChange('name', 'Lorem ipsum');
+    expect(dispatch).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call $$documentsFetch when calling onFilterChange', () => {
+    expect($$documentsFetch).not.toBeCalled();
+    instance.onFilterChange('name', 'Lorem ipsum');
+    setTimeout(() => {
+      expect($$documentsFetch).toHaveBeenCalledTimes(1);
+    }, 100);
   });
 });
